dd9807990a1a3750f85e8738a193ffb4b658dc0e
Vanessa rose Consigna <vconsigna.intern.integratr@outlook.com>
2023-03-21 11:32:31 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+index.php > html <body>
+    <html>
+        <body>
+        BSIT 
+$a1=array ("a"=> "BSBA" , "b" => HMO
+HMO
+NC2
+ENGINEERING
+NURSE
+ARCHITECT
+TOURISM
+        <?php>
+        $a1=array
+</body>
+
+</html>
